23cc3091c07bc737671a5be54edf8fd4925a26e9
I542324 <s.sarmah@sap.com>
2024-11-18 23:27:19 +0530

    Publishing
    Change-Id: I8bfebe144491f230567f3e14cb3ee5102f608008
    Signed-off-by: I542324 <s.sarmah@sap.com>

--- src/main/java/northwind/app/Runner.java
+++ src/main/java/northwind/app/Runner.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.nio.charset.Charset;
 
 import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,24 +16,35 @@ import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import northwind.config.KafkaConfig;
+import northwind.event.publisher.OrderPublisher;
 import northwind.models.Order;
 
 @Import(KafkaConfig.class)
 @SpringBootApplication
 @Component
 public class Runner implements ApplicationRunner {
+	
+	@Autowired
+	private OrderPublisher orderPublisher;
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(Runner.class);
 	}
 
 	@Override
-	public void run(ApplicationArguments args) throws Exception {
+	public void run(ApplicationArguments args)  {
 		ObjectMapper objectMapper = new ObjectMapper();
 		InputStream in = Runner.class.getClassLoader().getResourceAsStream("order.json");
-		String orderJson = IOUtils.toString(in,Charset.defaultCharset());
-		Order order = objectMapper.readValue(orderJson, Order.class);
-		System.out.println(order);
+		String orderJson;
+		try {
+			orderJson = IOUtils.toString(in,Charset.defaultCharset());
+			Order order = objectMapper.readValue(orderJson, Order.class);
+			orderPublisher.publishOrder(order);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		
 	}
 
